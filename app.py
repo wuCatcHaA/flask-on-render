7e37c5c4e240df5c5182965e05d17542c7a4aa21
Chadarat Imsarapang <chadarat.i@kkumail.com>
2025-02-21 21:43:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask on Render!"
+    return "Hello, Flask on Render! ไอ้บลัด"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
